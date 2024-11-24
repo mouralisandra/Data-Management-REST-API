89c3211123c6788a502bd2c8c1caecb7afcdef9e
Sandra Mourali <sandra.mourali@insat.ucar.tn>
2024-11-24 19:47:52 +0100

    2services are now separated

--- DocumentsService/documents/data.go
+++ DocumentsService/documents/data.go
@@ -8,8 +8,8 @@ import (
 // Function to initialize the Document service (optional)
 func Init() {
 	// Pre-populating with some document
-	documents = append(documents, Document{ID: "1", Isbn: "438227", Title: "Document1", Author: &Author{Firstname: "Sandra", Lastname: "Mourali"}})
-	documents = append(documents, Document{ID: "2", Isbn: "454555", Title: "Document2", Author: &Author{Firstname: "Nada", Lastname: "Mankai"}})
+	documents = append(documents, Document{ID: "1", Isbn: "438227", Title: "Content of Document1", Author: &Author{Firstname: "Sandra", Lastname: "Mourali"}})
+	documents = append(documents, Document{ID: "2", Isbn: "454555", Title: "Content of Document2", Author: &Author{Firstname: "Nada", Lastname: "Mankai"}})
 }
 
 // Function to setup routes for the Document service
